71df50470637f46a7d1037bb849754423dfc75bc
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2020-01-08 15:32:01 +0100

    Add basic file structure

---
+++ main.py
@@ -0,0 +1,12 @@
+import torch
+import torchsummary
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+import Model
+import Dataset
+import ModelWrapper
+
+if __name__ == '__main__':
+    pass
