123ad903731e75eb3e9825f225a724782007a28f
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2020-01-08 20:34:35 +0100

    Add getter function for different downsampling operations

--- Misc.py
+++ Misc.py
@@ -30,15 +30,37 @@ def get_activation(activation: str) -> nn.Sequential:
 def get_normalization_3d(normalization: str, channels: int) -> nn.Sequential():
     """
     Method to return different types of normalization operations
-    :param normalization: (str) Type of normalization
+    :param normalization: (str) Type of normalization ('batchnorm', 'instancenorm')
     :param channels: (int) Number of channels to use
     :return: (nn.Sequential) Normalization operation
     """
     assert normalization in ['batchnorm', 'instancenorm'], \
-        'Normalization {} is not avalible!'.format(normalization)
+        'Normalization {} is not available!'.format(normalization)
     if normalization == 'batchnorm':
         return nn.Sequential(nn.BatchNorm3d(channels))
     elif normalization == 'instancenorm':
         return nn.Sequential(nn.InstanceNorm3d(channels))
     else:
-        raise RuntimeError('Normalization {} is not avalible!'.format(normalization))
+        raise RuntimeError('Normalization {} is not available!'.format(normalization))
+
+
+def get_downsampling_3d(downsampling: str, factor: str = 2, channels: int = 0) -> nn.Sequential():
+    """
+    Method to return different types of downsampling operations
+    :param downsampling: (str) Type of donwsnapling ('maxpool', 'averagepool', 'convolution')
+    :param factor: (int) Factor of downsampling
+    :param channels: (int) Number of channels (only for convolution)
+    :return: (nn.Sequential) Downsampling operation
+    """
+    assert downsampling in ['maxpool', 'averagepool', 'convolution'], \
+        'Downsampling {} is not available'.format(downsampling)
+    if downsampling == 'maxpool':
+        return nn.Sequential(nn.MaxPool3d(kernel_size=factor, stride=factor))
+    elif downsampling == 'averagepool':
+        return nn.Sequential(nn.AvgPool3d(kernel_size=factor, stride=factor))
+    elif downsampling == 'convolution':
+        return nn.Sequential(
+            nn.Conv3d(in_channels=channels, out_channels=channels, kernel_size=factor, stride=factor, padding=0,
+                      bias=True))
+    else:
+        raise RuntimeError('Downsampling {} is not available'.format(downsampling))
