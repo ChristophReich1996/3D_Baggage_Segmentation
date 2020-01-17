92ccc8bdd3904930bf924866182cb5eda46f5381
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2020-01-17 14:02:01 +0100

    Fix DataParallel bug

--- main.py
+++ main.py
@@ -3,26 +3,27 @@ from torch.nn import BCELoss
 import torchsummary
 import numpy as np
 import matplotlib.pyplot as plt
+from torch.utils.data.dataloader import DataLoader
 import os
 
 import Models
 import Datasets
 import ModelWrapper
 
-
 if __name__ == '__main__':
+    # Batch size has to be a factor of the number of devices used in data parallel
     os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1, 3"
     model = Models.OccupancyNetwork()
-    # model = torch.nn.DataParallel(model)
-#     model(torch.rand([2, 1, 80, 52, 77]), torch.rand([2 ** 15, 3]))
+    model = torch.nn.DataParallel(model)
     ModelWrapper.OccupancyNetworkWrapper(occupancy_network=model,
-                                         occupancy_network_optimizer=torch.optim.Adam(model.parameters(),lr=1e-05),
-                                         training_data=Datasets.WeaponDataset(
-                                                target_path_volume="/fastdata/Smiths_LKA_Weapons/len_8/",
-                                                target_path_label="/fastdata/Smiths_LKA_Weapons/len_1/",
-                                                npoints=2**14,
-                                                side_len=8,
-                                                length=2600),
+                                         occupancy_network_optimizer=torch.optim.Adam(model.parameters(), lr=1e-05),
+                                         training_data=DataLoader(Datasets.WeaponDataset(
+                                             target_path_volume="/fastdata/Smiths_LKA_Weapons/len_8/",
+                                             target_path_label="/fastdata/Smiths_LKA_Weapons/len_1/",
+                                             npoints=2 ** 14,
+                                             side_len=8,
+                                             length=2600),
+                                             batch_size=9, shuffle=True),
                                          validation_data=None,
                                          test_data=None,
                                          loss_function=BCELoss(reduction='mean')
