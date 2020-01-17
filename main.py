2a5c3a6d2881f4f1aa793e0d89f1b075c164fa39
memmelma <marius.memmel@gmx.net>
2020-01-17 12:44:13 +0100

    added debugging devices

--- main.py
+++ main.py
@@ -8,18 +8,21 @@ import matplotlib.pyplot as plt
 import Models
 import Datasets
 import ModelWrapper
+ 
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 if __name__ == '__main__':
     model = Models.OccupancyNetwork()
-    model(torch.rand([2, 1, 80, 52, 77]), torch.rand([2 ** 15, 3]))
-#     ModelWrapper.OccupancyNetworkWrapper(occupancy_network=model,
-#                                          occupancy_network_optimizer=torch.optim.Adam(model.parameters(),lr=1e-05),
-#                                          training_data=Datasets.WeaponDataset(
-#                                                 target_path="/fastdata/Smiths_LKA_Weapons/len_1/",
-#                                                 npoints=2**14,
-#                                                 side_len=8,
-#                                                 length=2600),
-#                                          validation_data=None,
-#                                          test_data=None,
-#                                          loss_function=BCELoss(reduction='mean')
-#                                          ).train(epochs=2)
+#     model(torch.rand([2, 1, 80, 52, 77]), torch.rand([2 ** 15, 3]))
+    ModelWrapper.OccupancyNetworkWrapper(occupancy_network=model,
+                                         occupancy_network_optimizer=torch.optim.Adam(model.parameters(),lr=1e-05),
+                                         training_data=Datasets.WeaponDataset(
+                                                target_path="/fastdata/Smiths_LKA_Weapons/len_8/",
+                                                npoints=2**14,
+                                                side_len=8,
+                                                length=2600),
+                                         validation_data=None,
+                                         test_data=None,
+                                         loss_function=BCELoss(reduction='mean')
+                                         ).train(epochs=2)
