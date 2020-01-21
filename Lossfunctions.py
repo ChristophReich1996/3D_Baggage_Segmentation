c16c2bbae6674df15b8feaa600d95aa6c83cfc3d
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2020-01-21 13:17:28 +0100

    Add dice loss comments

--- Lossfunctions.py
+++ Lossfunctions.py
@@ -7,13 +7,13 @@ def dice_loss(prediction: torch.tensor, label: torch.tensor, smooth: float = 1.0
     :param prediction: (torch.tensor) Prediction tensor
     :param label: (torch.tensor) Label tensor
     :param smooth: (float) Smoothness factor of the dice loss
-    :return:
+    :return: (torch.tensor) Dice loss
     """
     # Flatten prediction and label
     prediction = prediction.view(-1)
     label = label.view(-1)
     # Calc loss
     intersect = torch.sum((prediction * label) + smooth)
-    union = torch.sum(pred) + torch.sum(label) + smooth
+    union = torch.sum(prediction) + torch.sum(label) + smooth
     loss = 1.0 - ((2.0 * intersect) / (union))
     return loss
