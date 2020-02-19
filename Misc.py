Bug fixes for faster training and implementation of instance normalization for 2D Tensor of shape (batch size, features) without momentum and learnable parameters

Misc.py
@@ -8,7 +8,6 @@ from pykdtree.kdtree import KDTree
 
 import ModelParts
 
-
 def intersection_over_union_bounding_box(prediction: torch.tensor, coordinates: torch.tensor, label: torch.tensor,
                                          threshold: float = 0.5,
                                          offset: torch.tensor = torch.tensor([0.0, 0.0, 0.0])) -> torch.tensor:
@@ -170,7 +169,7 @@ def get_activation(activation: str) -> nn.Sequential:
         raise RuntimeError('Activation {} is not available!'.format(activation))
 
 
-def get_normalization_3d(normalization: str, channels: int) -> nn.Sequential:
+def get_normalization_3d(normalization: str, channels: int, affine: bool = True) -> nn.Sequential:
     """
     Method to return different types of 3D normalization operations
     :param normalization: (str) Type of normalization ('batchnorm', 'instancenorm')
@@ -180,14 +179,14 @@ def get_normalization_3d(normalization: str, channels: int) -> nn.Sequential:
     assert normalization in ['batchnorm', 'instancenorm'], \
         'Normalization {} is not available!'.format(normalization)
     if normalization == 'batchnorm':
-        return nn.Sequential(nn.BatchNorm3d(channels))
+        return nn.Sequential(nn.BatchNorm3d(channels, affine=affine))
     elif normalization == 'instancenorm':
-        return nn.Sequential(nn.InstanceNorm3d(channels))
+        return nn.Sequential(nn.InstanceNorm3d(channels, affine=affine))
     else:
         raise RuntimeError('Normalization {} is not available!'.format(normalization))
 
 
-def get_normalization_1d(normalization: str, channels: int, channels_latent: int = None) -> Union[
+def get_normalization_1d(normalization: str, channels: int, channels_latent: int = None, affine: bool = True) -> Union[
     nn.Sequential, nn.Module]:
     """
     Method to return different types of 1D normalization operations
@@ -198,9 +197,9 @@ def get_normalization_1d(normalization: str, channels: int, channels_latent: int
     assert normalization in ['batchnorm', 'instancenorm', 'cbatchnorm', 'none'], \
         'Normalization {} is not available!'.format(normalization)
     if normalization == 'batchnorm':
-        return nn.Sequential(nn.BatchNorm1d(channels))
+        return nn.Sequential(nn.BatchNorm1d(channels, affine=affine))
     elif normalization == 'instancenorm':
-        return nn.Sequential(nn.InstanceNorm1d(channels))
+        return nn.Sequential(ModelParts.InstanceNorm1d())
     elif normalization == 'cbatchnorm':
         return ModelParts.ConditionalBatchNorm1d(channels_latent, channels)
     elif normalization == 'none':

ModelParts.py
@@ -154,27 +154,24 @@ class CoordinatesFullyConnectedBlock(nn.Module):
 
 class ConditionalBatchNorm1d(nn.Module):
     """
-    Implementation of a conditional batch normalization module using 1D convolutions to predict gamma and beta
+    Implementation of a conditional batch normalization module using linear operation to predict gamma and beta
     """
 
-    def __init__(self, latent_channels: int, output_channels: int, kernel_size: int = 1, bias: bool = True) -> None:
+    def __init__(self, latent_channels: int, output_channels: int, bias: bool = True,
+                 normalization: str = 'instancenorm') -> None:
         """
         Conditional batch normalization module including two 1D convolutions to predict gamma end beta
         :param latent_channels: (int) Features of the latent vector
         :param output_channels: (int) Features of the output vector to be normalized
-        :param kernel_size: (int) Kernel size used in 1d convolution
-        :param bias: (int) True if bias should be used in 2D convolutions
+        :param bias: (int) True if bias should be used in linear layer
+        :param normalization: (str) Type of normalization to normalize feature tensor before using gamma and  beta
         """
         super(ConditionalBatchNorm1d, self).__init__()
         # Init operations
-        self.convolution_gamma = nn.Conv1d(in_channels=latent_channels, out_channels=output_channels,
-                                           kernel_size=kernel_size,
-                                           bias=bias, padding=kernel_size // 2)
-        self.convolution_beta = nn.Conv1d(in_channels=latent_channels, out_channels=output_channels,
-                                          kernel_size=kernel_size,
-                                          bias=bias, padding=kernel_size // 2)
-        self.normalization = nn.BatchNorm1d(num_features=output_channels,
-                                            affine=False)  # affine=False -> gamma & beta not used
+        self.linear_gamma = nn.Linear(in_features=latent_channels, out_features=output_channels, bias=bias)
+        self.linear_beta = nn.Linear(in_features=latent_channels, out_features=output_channels, bias=bias)
+        self.normalization = Misc.get_normalization_1d(normalization=normalization, channels=output_channels,
+                                                       affine=False)  # affine=False -> gamma & beta not used
         # Reset parameters of convolutions
         self.reset_parameters()
 
@@ -182,10 +179,10 @@ class ConditionalBatchNorm1d(nn.Module):
         """
         Method resets the parameter of the convolution to predict gamma and beta
         """
-        nn.init.zeros_(self.convolution_gamma.weight)
-        nn.init.zeros_(self.convolution_beta.weight)
-        nn.init.ones_(self.convolution_gamma.bias)
-        nn.init.zeros_(self.convolution_beta.bias)
+        nn.init.zeros_(self.linear_gamma.weight)
+        nn.init.zeros_(self.linear_beta.weight)
+        nn.init.ones_(self.linear_gamma.bias)
+        nn.init.zeros_(self.linear_beta.bias)
 
     def forward(self, input: torch.Tensor, latent_vector: torch.Tensor) -> torch.Tensor:
         """
@@ -194,12 +191,9 @@ class ConditionalBatchNorm1d(nn.Module):
         :param latent_vector: (torch.Tensor) Latent vector tensor of shape (batch_size, features)
         :return: (torch.Tensor) Normalized tensor
         """
-        # Add third dimension for 1d convolution if needed
-        if len(latent_vector.size()) == 2:
-            latent_vector.unsqueeze_(2)
         # Perform convolutions to estimate gamma and beta
-        gamma = self.convolution_gamma(latent_vector)[:, :, 0]  # Remove last dimension
-        beta = self.convolution_beta(latent_vector)[:, :, 0]  # Remove last dimension
+        gamma = self.linear_gamma(latent_vector)
+        beta = self.linear_beta(latent_vector)
         # Perform normalization
         output_normalized = self.normalization(input)
         # Repeat gamma and beta to apply factors to every coordinate
@@ -208,3 +202,16 @@ class ConditionalBatchNorm1d(nn.Module):
         # Add factors
         output = gamma * output_normalized + beta
         return output
+
+
+class InstanceNorm1d(nn.Module):
+    """
+    Implementation of instance normalization for a 2D tensor of shape (batch size, features)
+    """
+
+    def __init__(self) -> None:
+        # Call super constructor
+        super(InstanceNorm1d, self).__init__()
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        return (input - input.mean(dim=1, keepdim=True)) / input.std(dim=1, keepdim=True)

ModelWrapper.py
@@ -107,7 +107,7 @@ class OccupancyNetworkWrapper(object):
         # Calc no grads
         with torch.no_grad():
             # Get data
-            for volume, coordinates, labels, actual in self.test_data:
+            for volume, coordinates, labels, actual in self.validation_data:
                 # Add batch size dim to data and to device
                 volume = volume.to(self.device)
                 coordinates = coordinates.to(self.device)

Models.py
@@ -14,26 +14,26 @@ class OccupancyNetwork(nn.Module):
 
     def __init__(self, number_of_encoding_blocks: int = 5,  # 5,  # Encoding path parameters
                  channels_in_encoding_blocks: List[Tuple[int]] =
-                 [(1, 32), (32, 32), (32, 32), (32, 32), (32, 5)],
+                 [(1, 64), (64, 64), (64, 64), (64, 64), (64, 5)],
                  #   [(1, 32), (32, 64), (64, 128),(128, 64), (64, 32), (32, 3)],
                  #   [(1, 64), (64, 128), (128, 256),(256, 128), (128, 64), (64, 3)],
                  kernel_size_encoding: Union[int, List[int]] = 3, stride_encoding: Union[int, List[int]] = 1,
                  padding_encoding: Union[int, List[int]] = 1,
-                 activation_encoding: Union[str, List[str]] = 'prelu',
+                 activation_encoding: Union[str, List[str]] = 'selu',
                  downsampling_encoding: Union[str, List[str]] =
                  #   ['none', 'averagepool', 'averagepool', 'averagepool', 'averagepool'],
-                 ['averagepool', 'averagepool', 'averagepool', 'maxpool', 'none'],
+                 ['averagepool', 'averagepool', 'averagepool', 'averagepool', 'none'],
                  downsampling_factor_encoding: Union[int, List[int]] = 2,
-                 normalization_encoding: Union[str, List[str]] = 'batchnorm',
+                 normalization_encoding: Union[str, List[str]] = 'instancenorm',
                  dropout_rate_encoding: Union[float, List[float]] = 0.0,
-                 bias_encoding: Union[bool, List[bool]] = True,
+                 bias_encoding: Union[bool, List[bool]] = False,
                  number_of_decoding_blocks: int = 5,  # Decoding path parameter
                  channels_in_decoding_blocks: List[Tuple[int]] =
-                 [(300 + 3, 128), (128, 128), (128, 128), (128, 128), (128, 128)],
+                 [(300 + 3, 256), (256, 256), (256, 256), (256, 256), (256, 256)],
                  #  [(180 + 3, 256), (256, 256), (256, 256), (256, 256), (256, 256), (256, 1)],
                  activation_decoding: Union[str, List[str]] = 'selu',
                  normalization_decoding: Union[str, List[str]] = 'cbatchnorm',
-                 dropout_rate_decoding: Union[float, List[float]] = [0.025, 0.0, 0.0, 0.0, 0.0],
+                 dropout_rate_decoding: Union[float, List[float]] = [0.0, 0.0, 0.0, 0.0, 0.0],
                  bias_decoding: Union[bool, List[bool]] = True,
                  output_activation: str = 'sigmoid') -> None:
         """
@@ -145,6 +145,153 @@ class OccupancyNetwork(nn.Module):
                 output_decoding = block(input_decoding, output_encoding_flatten.clone())
             else:
                 output_decoding = block(output_decoding, output_encoding_flatten.clone())
+        # Perform last linear layer + sigmoid activation
+        output = self.output_block(output_decoding)
+        return output
+
+from typing import List, Tuple, Union
+
+import torch
+import torch.nn as nn
+
+import Misc
+import ModelParts
+
+
+class OccupancyNetworkNoCat(nn.Module):
+    """
+    Implementation of an occupancy network for binary classification of a 3D volume with no concatenation of latent tensor
+    """
+
+    def __init__(self, number_of_encoding_blocks: int = 5,  # 5,  # Encoding path parameters
+                 channels_in_encoding_blocks: List[Tuple[int]] =
+                 [(1, 64), (64, 64), (64, 64), (64, 64), (64, 5)],
+                 #   [(1, 32), (32, 64), (64, 128),(128, 64), (64, 32), (32, 3)],
+                 #   [(1, 64), (64, 128), (128, 256),(256, 128), (128, 64), (64, 3)],
+                 kernel_size_encoding: Union[int, List[int]] = 3, stride_encoding: Union[int, List[int]] = 1,
+                 padding_encoding: Union[int, List[int]] = 1,
+                 activation_encoding: Union[str, List[str]] = 'leaky relu',
+                 downsampling_encoding: Union[str, List[str]] =
+                 #   ['none', 'averagepool', 'averagepool', 'averagepool', 'averagepool'],
+                 ['averagepool', 'averagepool', 'averagepool', 'averagepool', 'none'],
+                 downsampling_factor_encoding: Union[int, List[int]] = 2,
+                 normalization_encoding: Union[str, List[str]] = 'instancenorm',
+                 dropout_rate_encoding: Union[float, List[float]] = 0.0,
+                 bias_encoding: Union[bool, List[bool]] = False,
+                 number_of_decoding_blocks: int = 5,  # Decoding path parameter
+                 channels_in_decoding_blocks: List[Tuple[int]] =
+                 [(3, 256), (256, 256), (256, 256), (256, 256), (256, 256)],
+                 #  [(180 + 3, 256), (256, 256), (256, 256), (256, 256), (256, 256), (256, 1)],
+                 activation_decoding: Union[str, List[str]] = 'selu',
+                 normalization_decoding: Union[str, List[str]] = 'cbatchnorm',
+                 dropout_rate_decoding: Union[float, List[float]] = [0.0, 0.0, 0.0, 0.0, 0.0],
+                 bias_decoding: Union[bool, List[bool]] = True,
+                 output_activation: str = 'sigmoid') -> None:
+        """
+        Constructor method
+        :param number_of_encoding_blocks: (int) Number of blocks in encoding path
+        :param channels_in_encoding_blocks: (List[Tuple[int]]) Number of input and output channels of each encoding block
+        :param kernel_size_encoding: (int, List[int]) Kernel size of each convolution in each encoding block
+        :param stride_encoding: (int, List[int]) Stride of each convolution in each encoding block
+        :param padding_encoding: (int, List[int]) Padding of each convolution in each encoding block
+        :param activation_encoding: (str, List[str]) Activation used in each encoding block
+        :param downsampling_encoding: (str, List[str]) Downsampling operation in each encoding block
+        :param downsampling_factor_encoding: (int, List[int]) Downsampling factor in each encoding block
+        :param normalization_encoding: (str, List[str]) Type of normalization use in each encoding block
+        :param dropout_rate_encoding: (float, List[float]) Dropout rate to perform in each encoding block
+        :param bias_encoding: (bool, List[bool]) Use bias in each convolution in each encoding block
+        :param number_of_decoding_blocks: (int) Number of block in decoding path
+        :param channels_in_decoding_blocks: (List[Tuple[int]]) Number of input and output channels of each decoding block
+        :param activation_decoding: (str, List[str]) Activation used in each decoding block
+        :param normalization_decoding: (str, List[str]) Type of normalization use in each decoding block
+        :param dropout_rate_decoding: (float, List[float]) Dropout rate to perform in each decoding block
+        :param bias_decoding: (bool, List[bool]) Use bias in each convolution in each decoding block
+        :param bias_residual_decoding: (bool, List[bool]) Use bias in residual mapping in each decoding block
+        :param output_activation: (str) Type of activation function used for output
+        """
+        # Call super constructor
+        super(OccupancyNetworkNoCat, self).__init__()
+        # Convert encoding parameters to lists
+        channels_in_encoding_blocks = Misc.parse_to_list(channels_in_encoding_blocks, number_of_encoding_blocks,
+                                                         'channels in encoding blocks')
+        kernel_size_encoding = Misc.parse_to_list(kernel_size_encoding, number_of_encoding_blocks,
+                                                  'kernel size encoding')
+        stride_encoding = Misc.parse_to_list(stride_encoding, number_of_encoding_blocks,
+                                             'stride encoding')
+        padding_encoding = Misc.parse_to_list(padding_encoding, number_of_encoding_blocks,
+                                              'padding encoding')
+        activation_encoding = Misc.parse_to_list(activation_encoding, number_of_encoding_blocks,
+                                                 'activation encoding')
+        downsampling_encoding = Misc.parse_to_list(downsampling_encoding, number_of_encoding_blocks,
+                                                   'downsampling encoding')
+        downsampling_factor_encoding = Misc.parse_to_list(downsampling_factor_encoding, number_of_encoding_blocks,
+                                                          'downsampling factor encoding')
+        normalization_encoding = Misc.parse_to_list(normalization_encoding, number_of_encoding_blocks,
+                                                    'normalization encoding')
+        dropout_rate_encoding = Misc.parse_to_list(dropout_rate_encoding, number_of_encoding_blocks,
+                                                   'dropout rate encoding')
+        bias_encoding = Misc.parse_to_list(bias_encoding, number_of_encoding_blocks,
+                                           'bias encoding')
+        # Convert decoding parameters to lists
+        channels_in_decoding_blocks = Misc.parse_to_list(channels_in_decoding_blocks, number_of_decoding_blocks,
+                                                         'channels in decoding blocks')
+        activation_decoding = Misc.parse_to_list(activation_decoding, number_of_decoding_blocks,
+                                                 'activation decoding')
+        normalization_decoding = Misc.parse_to_list(normalization_decoding, number_of_decoding_blocks,
+                                                    'normalization decoding')
+        dropout_rate_decoding = Misc.parse_to_list(dropout_rate_decoding, number_of_decoding_blocks,
+                                                   'dropout rate decoding')
+        bias_decoding = Misc.parse_to_list(bias_decoding, number_of_decoding_blocks,
+                                           'bias decoding')
+
+        # Init encoding blocks
+        self.encoding = nn.Sequential(*[ModelParts.VolumeEncoderBlock(
+            input_channels=channels_in_encoding_blocks[index][0],
+            output_channels=channels_in_encoding_blocks[index][1],
+            kernel_size=kernel_size_encoding[index],
+            stride=stride_encoding[index],
+            padding=padding_encoding[index],
+            activation=activation_encoding[index],
+            downsampling=downsampling_encoding[index],
+            downsampling_factor=downsampling_factor_encoding[index],
+            normalization=normalization_encoding[index],
+            dropout_rate=dropout_rate_encoding[index],
+            bias=bias_encoding[index])
+            for index in range(number_of_encoding_blocks)])
+
+        # Init decoding blocks
+        self.decoding = nn.ModuleList()
+        for index in range(number_of_decoding_blocks):
+            self.decoding.append(ModelParts.CoordinatesFullyConnectedBlock(
+                input_channels=channels_in_decoding_blocks[index][0],
+                output_channels=channels_in_decoding_blocks[index][1],
+                activation=activation_decoding[index],
+                normalization=normalization_decoding[index],
+                dropout_rate=dropout_rate_decoding[index],
+                bias=bias_decoding[index]))
+
+        # Init output activation
+        self.output_block = nn.Sequential(
+            nn.Linear(in_features=channels_in_decoding_blocks[-1][1], out_features=1, bias=True),
+            Misc.get_activation(output_activation))
+
+    def forward(self, volume: torch.tensor, coordinates: torch.tensor) -> torch.tensor:
+        """
+        Forward pass of the occupancy network
+        :param volume: (torch.tensor) Input tensor including 3D volume
+        :param coordinates: (torch.tensor) Input tensor including coordinates
+        :return: (torch.tensor) Output tensor
+        """
+        # Perform encoding path
+        output_encoding = self.encoding(volume)
+        # Flatten latent vector for decoding path
+        output_encoding_flatten = output_encoding.view(output_encoding.shape[0], -1)
+        # Perform decoding path
+        for index, block in enumerate(self.decoding):
+            if index == 0:
+                output_decoding = block(coordinates, output_encoding_flatten)
+            else:
+                output_decoding = block(output_decoding, output_encoding_flatten)
         # Perform last linear layer + sigmoid activation
         output = self.output_block(output_decoding)
         return output

main.py
@@ -14,9 +14,9 @@ import Misc
 
 if __name__ == '__main__':
     # Batch size has to be a factor of the number of devices used in data parallel
-    os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1, 2'  # "0, 1, 3, 4, 5"
+    os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1, 3, 4, 5'  # "0, 1, 3, 4, 5"
     # Init model
-    model = torch.nn.DataParallel(Models.OccupancyNetwork().cuda())
+    model = torch.nn.DataParallel(Models.OccupancyNetworkNoCat().cuda())  # Models.OccupancyNetwork().cuda()
     # Print model
     print(model)
     # Print number of parameters included in the model
@@ -32,19 +32,19 @@ if __name__ == '__main__':
                                                              target_path_label='/fastdata/Smiths_LKA_Weapons_Down/len_1/',
                                                              npoints=2 ** 14,
                                                              side_len=8,
-                                                             length=2580),
-                                                             batch_size=30, shuffle=True,
+                                                             length=2600),
+                                                             batch_size=25, shuffle=True,
                                                              collate_fn=Misc.many_to_one_collate_fn_sample,
-                                                             num_workers=30, pin_memory=True),
+                                                             num_workers=25, pin_memory=True),
                                                          test_data=DataLoader(Datasets.WeaponDataset(
                                                              target_path_volume='/fastdata/Smiths_LKA_Weapons_Down/len_8/',
                                                              target_path_label='/fastdata/Smiths_LKA_Weapons_Down/len_1/',
-                                                             npoints=2 ** 15,
+                                                             npoints=2 ** 17,
                                                              side_len=8,
-                                                             length=276,  # 200,
-                                                             offset=2580,  # 2600,
+                                                             length=306,  # 200,
+                                                             offset=2600,  # 2600,
                                                              test=True,
-                                                             share_box=0),
+                                                             share_box=0.0),
                                                              batch_size=1, shuffle=True,
                                                              collate_fn=Misc.many_to_one_collate_fn_sample_down,
                                                              num_workers=1, pin_memory=True,
@@ -52,12 +52,13 @@ if __name__ == '__main__':
                                                          validation_data=DataLoader(Datasets.WeaponDataset(
                                                              target_path_volume='/fastdata/Smiths_LKA_Weapons_Down/len_8/',
                                                              target_path_label='/fastdata/Smiths_LKA_Weapons_Down/len_1/',
-                                                             npoints=2 ** 17,
+                                                             npoints=2 ** 14,
                                                              side_len=8,
-                                                             length=100,  # 200,
-                                                             offset=2856,  # 2600,
+                                                             length=50,  # 200,
+                                                             offset=2906,  # 2600,
                                                              test=True,
-                                                             share_box=0),
+                                                             share_box=0.0,
+                                                             sampling='one_fast'),
                                                              batch_size=1, shuffle=True,
                                                              collate_fn=Misc.many_to_one_collate_fn_sample_down,
                                                              num_workers=1, pin_memory=True,
@@ -65,7 +66,7 @@ if __name__ == '__main__':
                                                          loss_function=torch.nn.BCELoss(reduction='mean'),
                                                          device='cuda')
 
-    model_wrapper.train(epochs=100, model_save_path='/visinf/home/vilab15/Projects/3D_baggage_segmentation/')
+    model_wrapper.train(epochs=200, model_save_path='/visinf/home/vilab15/Projects/3D_baggage_segmentation/')
     model_wrapper.test(threshold=0.5, offset=torch.tensor([0.0, 0.0, 0.0]))  # Best offset=10 threshold=0.8
 
     # model = torch.load('/visinf/home/vilab16/3D_baggage_segmentation/' + 'occupancy_network_lo_lo_dice_cuda.pt').module