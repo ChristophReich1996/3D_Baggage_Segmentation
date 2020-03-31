73b9867a9db6dda662a8e5b91a09501aede899b0
nwWag <nicolas_wagner@gmx.de>
2020-03-31 15:40:42 +0200

    Delete run_unet.sh

--- run_unet.sh
+++
@@ -1,3 +0,0 @@
-env CUDA_VISIBLE_DEVICES=3 python train_pipeline_unet.py \
-        -sl 32 -sld 16 -df 2 -lr 3 -ct fifo -cr focal -l False \
-        -n _hilounet_32_16_2_3_fifo_focal_wide_rtx > logs/hilounet_32_16_2_3_fifo_focal_wide_rtx.txt
