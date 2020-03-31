795f4f5c79276c668c7c723b26f3042c4aa1b344
nwWag <nicolas_wagner@gmx.de>
2020-03-31 15:39:59 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#env CUDA_VISIBLE_DEVICES=0  python train_pipeline.py \
-#    -sl 48 -sld 24 -df 2 -np 10 -lr 4 -ct fifo -cr bce -l False \
-#    -n _hilo_48_24_2_10_4_fifo_bce > logs/hilo_48_24_2_10_4_fifo_bce.txt
-env CUDA_VISIBLE_DEVICES=0,2  python train_pipeline.py \
-        -sl 16 -sld 8 -df 2 -np 10 -lr 3 -ct fifo -cr bce -l True \
-        -n _hilo_16_8_2_10_3_fifo_bce > logs/hilo_16_8_2_10_3_fifo_bce.txt
